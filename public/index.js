3ef9797d5d0139f35d8bbf0375b5a347ca194dc9
Prokop Havlík <maraxencz1097@gmail.com>
2023-01-25 23:31:03 +0100

    pridan footer, uprava cards

--- public/index.js
+++ public/index.js
@@ -1,3 +1,32 @@
 document.querySelector('#burger').addEventListener('click', () => {
     document.querySelector('#menu').classList.toggle('hidden');
 });
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+
+if (mediaQuery.matches) {
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    slidesPerGroup: 2,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+        1035: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+          slidesPerGroup: 3,
+          loop: false,
+        loopFillGroupWithBlank: true,
+        },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
